81da392d25391af0d7fb729d0bdf4396ba5da7fa
Estom Jr <48894573+EstomJr@users.noreply.github.com>
2022-10-09 23:54:05 -0300

    Atualização IAAD

--- atividade3.sql
+++ atividade3.sql
@@ -27,7 +27,7 @@ create table Medicos(
 create table Especialidade (
     CodEspec INT PRIMARY KEY AUTO_INCREMENT, 
     NomeEspec VARCHAR(255), 
-    Descricao VARCHAR(255)
+    Descricao VARCHAR(255) NOT NULL
 );
 
 create table Paciente(
@@ -45,7 +45,9 @@ create table ClinicaMedico(
     CodCli INT NOT NULL, 
     CodMed INT NOT NULL, 
     DataIngresso DATE, 
-    CargaHorariaSemanal DECIMAL(3,1)
+    CargaHorariaSemanal DECIMAL(3,1),
+    primary key (CodCli, CodMed)
+
 );
 
 create table AgendaConsulta(
@@ -57,40 +59,40 @@ create table AgendaConsulta(
 
 
 insert into Clinicas values
-    (10001,'Esperanca','Encruzilhada da agamenon', '8198395431', 'atendimento@esperanca.com'),
-    (Default,'HSCP','Volta da rua da lua no fantástico', '8729128312', 'atendimento@hscp.com'),
-    (Default,'HAPVITA', 'Avenida Marechal Capitão', '8185493212', 'atendimento@hapvita.com'),
-    (Default, 'Hospital das Clínicas','BR 101', '8912397124', 'atendimento@hspclinicas.gov'),
-    (Default, 'HOPE', 'Avenida Naufrago Ribeiro', '8157864778', 'atendimento@hope.com');
+    (10001,'Unimed','Ilha do leite', '8198395431', 'Unimed@Unimed.com'),
+    (Default,'Hapvida','Olinda', '8729128312', 'Hapvida@Hapvida.com'),
+    (Default,'Esperanca', 'Avenida janga', '8185493212', 'Esperanca@Esperanca.com'),
+    (Default, 'HR','Agamenom magalhaes', '8912397124', 'HR@HR.gov'),
+    (Default, 'Real Portugues', 'Boa viagem', '8157864778', 'Portugues@Portugues.com');
 
 insert into Medicos values
-	(20001, 'Dr. Roberto Claudio', 'M', '81997802526', 'RobertoClaudio@mail.com',30001),
-    (Default, 'Dra. Suelen Indigo', 'F', '81995481697', 'SuelenIndigo@mail.com',30002),
-    (Default, 'Dr. Enzo de Souza', 'M', '81988745995', 'EnzodeSouza@mail.com',30001),
-    (Default, 'Dr. Alfred Bane', 'M', '81992548790', 'AlfredBane@mail.com',30003),
-    (Default, 'Dr. Carlos Magno', 'M', '81987415212', 'CarlosMagno@mail.com',30004),
-    (Default, 'Dr. Leonidas Campelo', 'M', '81988745962', 'LeonidasCampelo@mail.com',30005),
-    (Default, 'Dra. Vania Lucinda', 'F', '81950214569', 'VaniaLucinda@mail.com',30006);
+	(20001, 'Dr. Estom Junior', 'M', '81997802526', 'Estom@mail.com',80001),
+    (Default, 'Dra. Karolayne Michele', 'F', '81995481697', 'Karolayne@mail.com',80002),
+    (Default, 'Dr. David Ramos', 'M', '81988745995', 'David@mail.com',80001),
+    (Default, 'Dr. Breno Gomes', 'M', '81992548790', 'Breno@mail.com',80003),
+    (Default, 'Dr. carlos souza', 'M', '81987415212', 'carlos@mail.com',80004),
+    (Default, 'Dr. EduarDo Lopes', 'M', '81988745962', 'EduarDo@mail.com',80005),
+    (Default, 'Dra. Daniela Lucinda', 'F', '81950214569', 'Daniela@mail.com',80006);
 
 insert into Especialidade values
-	(30001,'Ortopedia e traumatologia', 'atua no tratamento de problemas nos músculos, ossos e traumas'),
-    (Default,'Cardiologia', 'trata de doenças e problemas relacionados ao coração e à circulação sanguínea'),
-    (Default,'Oncologia', 'estuda e trata os diversos tipos de câncer'),
-    (Default,'Oftalmologia', 'Previne e trata doenças nas estruturas oculares'),
-    (Default,'Otorrinolaringologia', 'trata as doenças do ouvido, do nariz e da garganta'),
+	(30001,'Cardiologia ', 'especialista nas doenças do coração e seu sistema circulatório'),
+    (Default,'Dermatologia', ' cuida da prevenção e tratamento das doenças do maior órgão do corpo: a pele e seus apêndices'),
+    (Default,'Ginecologia ', 'cuida da saúde da mulher, desde sua adolescência até a idade madura, atuando na prevenção, no diagnóstico e no tratamento de diversas patologias.'),
+    (Default,'Anestesiologia', 'especialista responsável pela condução dos procedimentos anestésicos e pelo acompanhamento do paciente durante o período perioperatório'),
+    (Default,'Ortopedia', 'especialista no diagnóstico e tratamento das disfunções e lesões do sistema locomotor (músculos, ossos, ligamentos, nervos, articulações, tendões etc.).'),
     (Default,'Neurologia', 'trata dos aspectos clínicos dos problemas no sistema nervoso');
 
 insert into Paciente values
-	('08726654024', 'José', '2000-09-18', 'M', '2682878513', 'jose_21@mail.com'),
-    ('70875870058', 'Robson', '1997-05-2', 'M', '2292878513', 'Robson_89@mail.com'),
-    ('56916890029', 'Cassio', '1990-08-11', 'M', '8182878513', 'cassio_14@mail.com'),
-    ('80250103095', 'Pedro', '2004-01-16', 'M', '8115878513', 'peu_02@mail.com'),
-    ('99535751000', 'Tony', '2011-11-08', 'M', '1234784513', 'tony_12@mail.com'),
-    ('68253995008', 'Amalia', '2005-12-25', 'F', '8111978513', 'amalia_92@mail.com'),
-    ('54981742096', 'Eduarda', '1998-03-04', 'F', '2182878513', 'edu_arda@mail.com'),
-    ('15905244030', 'Penywise', '1980-02-25', 'M', '9856878513', 'acoisa@mail.com'),
-    ('78694926040', 'Joana', '2015-07-19', 'F', '2292898513', 'joan_a@mail.com'),
-    ('91527728021', 'Arthur', '1983-06-05', 'M', '9856878986', 'arthur_135@mail.com');
+	('08726654024', 'Dinaildo', '2000-09-18', 'M', '2682878513', 'Dinaildo@mail.com'),
+    ('70875870058', 'Breno', '1997-05-2', 'M', '2292878513', 'Breno@mail.com'),
+    ('56916890029', 'Dylan', '1990-08-11', 'M', '8182878513', 'Dylan@mail.com'),
+    ('80250103095', 'Anthony', '2004-01-16', 'M', '8115878513', 'Anthony@mail.com'),
+    ('99535751000', 'Bruk', '2011-11-08', 'M', '1234784513', 'Bruk@mail.com'),
+    ('68253995008', 'Josue', '2005-12-25', 'M', '8111978513', 'Josue@mail.com'),
+    ('54981742096', 'Elder', '1998-03-04', 'M', '2182878513', 'Elder@mail.com'),
+    ('15905244030', 'Ruth', '1980-02-25', 'F', '9856878513', 'Ruth@mail.com'),
+    ('78694926040', 'Libna', '2015-07-19', 'F', '2292898513', 'Libna@mail.com'),
+    ('91527728021', 'Lais', '1983-06-05', 'F', '9856878986', 'Lais@mail.com');
 
 insert into ClinicaMedico values
 	(10001, 20001, '2015-02-07', 20.0),
@@ -112,16 +114,14 @@ insert into AgendaConsulta values
     (10002, 20006, '99535751000', '2021-09-26 16:30:00'),
     (10005, 20004, '78694926040', '2022-10-02 17:00:00');
 
-alter table Medicos ADD FOREIGN KEY(CodEspec) REFERENCES Especialidade(CodEspec) ON DELETE CASCADE ON UPDATE CASCADE;
+ alter table Medicos ADD FOREIGN KEY(CodEspec) REFERENCES Especialidade(CodEspec) ON DELETE CASCADE ON UPDATE CASCADE;
 
 alter table AgendaConsulta ADD FOREIGN KEY (CpfPaciente) REFERENCES Paciente(CpfPaciente) ON DELETE RESTRICT;
-alter table AgendaConsulta ADD FOREIGN KEY (CodMed) REFERENCES Medicos(CodMed);
-alter table AgendaConsulta ADD FOREIGN KEY (CodCli) REFERENCES Clinicas(CodCli);
-
+alter table AgendaConsulta ADD FOREIGN KEY(CodCli) REFERENCES ClinicaMedico(CodCli);
+alter table AgendaConsulta ADD FOREIGN KEY(CodMed) REFERENCES ClinicaMedico(CodMed);
 alter table ClinicaMedico ADD FOREIGN KEY (CodCli) REFERENCES Clinicas(CodCli);
 alter table ClinicaMedico ADD FOREIGN KEY (CodMed) REFERENCES Medicos(CodMed);
 
-alter table Especialidade ADD FOREIGN KEY (CodEspec) REFERENCES Medicos(CodEspec) ON DELETE CASCADE ON UPDATE CASCADE;
 
 SET LOCAL lc_time_names = 'pt_BR'; 
  
